292b2991f62b388cf9f1ac2fffd00536d73c35a3
sijian.gu <sijian.gu@genewiz.com>
2017-09-14 17:23:47 +0800

    edit relationship

--- src/components/draggable/relationship.vue
+++ src/components/draggable/relationship.vue
@@ -1,13 +1,19 @@
 <template>
   <svg style="width: 100%;height: 100%;min-height: 800px;min-width: 800px">
     <template v-for="r in relationships">
+      <g fill="none" class="bn-line-action">
+        <rect :x="getActionPoint(r).x" :y="getActionPoint(r).y" width="50" height="50" fill="#ffffff"  transform="translate(0,-25)" />
+        <path pointer-events="all" version="1.1" xmlns="http://www.w3.org/2000/svg" :d="getActionPath(r)" class="" :stroke="lineColor" :fill="lineColor" transform="translate(3.5,3)" @click="removeRelationship(r)">
+        </path>
+        <text :x="getActionPoint(r).x" :y="getActionPoint(r).y" class="bn-icon bn-line-action-item" font-size="35" :fill="lineColor" transform="translate(15,-5)" >
+          &#xe6a1;
+        </text>
+        <text :x="getActionPoint(r).x" :y="getActionPoint(r).y" class="bn-icon bn-line-action-item" font-size="35" :fill="lineColor" transform="translate(15,20)"  @click="removeRelationship(r)">
+          &#xe60c;
+        </text>
+      </g>
       <path :d="getpath(r)" transform="translate(3.5,3)" pointer-events="visibleStroke" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="none" :stroke="lineColor" style="" stroke-width="1"></path>
       <path pointer-events="all" version="1.1" xmlns="http://www.w3.org/2000/svg" :d="getEndPoint(r)" class="" :stroke="lineColor" :fill="lineColor" transform="translate(3.5,3)"></path>
-      <g fill="none" class="bn-line-action">        
-      <path pointer-events="all" version="1.1" xmlns="http://www.w3.org/2000/svg" :d="getActionPath(r)" class="" :stroke="lineColor" :fill="lineColor" transform="translate(3.5,3)" @click="removeRelationship(r)">   
-      </path>  
-      <rect x="200" y="10" width="10" height="10" class="bn-line-action-item" />   
-      </g>
     </template>
   </svg>
 </template>
@@ -20,8 +26,8 @@ export default {
       orignalPosition: {},
       dw: 100,
       dh: 50,
-      offsetx:25,
-      offsetx2:5
+      offsetx: 25,
+      offsetx2: 5
     }
   },
   props: ['relationships'],
@@ -38,76 +44,82 @@ export default {
     getEndPoint(r) {
       return this.calcLine(r).arrow;
     },
-    getActionPath(r){
-      var m2= this.calcLine(r).m2
-      return `M ${m2.x} ${m2.y} A 5 5 0 1 0 ${m2.x} ${m2.y-1}`
+    getActionPoint(r) {
+      return this.calcLine(r).m2;
     },
-    removeRelationship(r){
-      var idx=r.from.next.indexOf(r.to.id);
-      r.from.next.splice(idx,1);
+    getActionPath(r) {
+      var m2 = this.calcLine(r).m2
+      return `M ${m2.x} ${m2.y} A 3 3 0 1 0 ${m2.x} ${m2.y-1}`
     },
-    calcLine(r){
-      var fx1=this.getLeft(r.from).x;
-      var fx2=this.getRight(r.from).x;
-      var tx1=this.getLeft(r.to).x;
-      var tx2=this.getRight(r.to).x;
+    removeRelationship(r) {
+      var idx = r.from.next.indexOf(r.to.id);
+      r.from.next.splice(idx, 1);
+    },
+    calcLine(r) {
+      var fx1 = this.getLeft(r.from).x;
+      var fx2 = this.getRight(r.from).x;
+      var tx1 = this.getLeft(r.to).x;
+      var tx2 = this.getRight(r.to).x;
 
-      var fy1=this.getTop(r.from).y;
-      var fy2=this.getBottom(r.from).y;
-      var ty1=this.getTop(r.to).y;
-      var ty2=this.getBottom(r.to).y;
-      var minoffset=35
-      if(fx2+minoffset<tx1){
+      var fy1 = this.getTop(r.from).y;
+      var fy2 = this.getBottom(r.from).y;
+      var ty1 = this.getTop(r.to).y;
+      var ty2 = this.getBottom(r.to).y;
+      var minoffset = 35
+      if (fx2 + minoffset < tx1) {
         return this.drawRightToLeft(r);
-      }else if(fx1>tx2+minoffset){
+      } else if (fx1 > tx2 + minoffset) {
         return this.drawLeftToRight(r);
-      }else if(fy1>ty2){
+      } else if (fy1 > ty2) {
         return this.drawTopToBottom(r);
-      }else if(fy2<ty2){
+      } else if (fy2 < ty2) {
         return this.drawBottomToTop(r);
       }
       return this.drawRightToLeft(r);
     },
     drawRightToLeft(r) {
-      var offsetx=this.offsetx;
+      var offsetx = this.offsetx;
       var from = this.getRight(r.from);
-      from.x+=offsetx;
+      from.x += offsetx;
       var to = this.getLeft(r.to);
-      to.x-=this.offsetx2;
+      to.x -= this.offsetx2;
       var m1 = { x: (to.x - from.x) / 2 + from.x, y: from.y };
       var m2 = { x: (to.x - from.x) / 2 + from.x, y: to.y };
       var arrow = `M${to.x},${to.y} L${to.x-10},${to.y+5} L${to.x-6.23},${to.y} L${to.x-10},${to.y-5} L${to.x},${to.y}`
-      return { line: `M ${from.x-5} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow
-      ,m2:m2 };
+      return {
+        line: `M ${from.x-5} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`,
+        arrow: arrow,
+        m2: m2
+      };
     },
     drawLeftToRight(r) {
-      var offsetx=this.offsetx;
+      var offsetx = this.offsetx;
       var from = this.getLeft(r.from);
       var to = this.getRight(r.to);
-      to.x+=offsetx;
+      to.x += offsetx;
       var m1 = { x: (to.x - from.x) / 2 + from.x, y: from.y };
       var m2 = { x: (to.x - from.x) / 2 + from.x, y: to.y };
       var arrow = `M${to.x},${to.y} L${to.x+10},${to.y+5} L${to.x+6.23},${to.y} L${to.x+10},${to.y-5} L${to.x},${to.y}`
-      return { line: `M ${from.x-5} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow ,m2:m2};
+      return { line: `M ${from.x-5} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow, m2: m2 };
     },
-    drawTopToBottom(r){
+    drawTopToBottom(r) {
       var from = this.getTop(r.from);
       var to = this.getBottom(r.to);
 
-      var m1 = { x: from.x, y: (to.y - from.y)*1.0 / 2.0 + from.y };
-      var m2 = { x: to.x, y: (to.y - from.y)*1.0 / 2.0 + from.y };
-      var offsety=15;
+      var m1 = { x: from.x, y: (to.y - from.y) * 1.0 / 2.0 + from.y };
+      var m2 = { x: to.x, y: (to.y - from.y) * 1.0 / 2.0 + from.y };
+      var offsety = 15;
       var arrow = `M${to.x},${to.y+offsety} L${to.x+5},${to.y+10+offsety} L${to.x},${to.y+6.23+offsety} L${to.x-5},${to.y+10+offsety} L${to.x},${to.y+offsety}`
-      return { line: `M ${from.x} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow ,m2:m2};
+      return { line: `M ${from.x} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow, m2: m2 };
     },
-    drawBottomToTop(r){
+    drawBottomToTop(r) {
       var from = this.getBottom(r.from);
       var to = this.getTop(r.to);
-      to.y-=this.offsetx2;
-      var m1 = { x: from.x, y: (to.y - from.y)*1.0 / 2.0 + from.y };
-      var m2 = { x: to.x, y: (to.y - from.y)*1.0 / 2.0 + from.y };
+      to.y -= this.offsetx2;
+      var m1 = { x: from.x, y: (to.y - from.y) * 1.0 / 2.0 + from.y };
+      var m2 = { x: to.x, y: (to.y - from.y) * 1.0 / 2.0 + from.y };
       var arrow = `M${to.x},${to.y} L${to.x-5},${to.y-10} L${to.x},${to.y-6.23} L${to.x+5},${to.y-10} L${to.x},${to.y}`
-      return { line: `M ${from.x} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow ,m2:m2};
+      return { line: `M ${from.x} ${from.y} L ${m1.x} ${m1.y} L ${m2.x} ${m2.y} L ${to.x} ${to.y}`, arrow: arrow, m2: m2 };
     },
     getSize(item) {
       var w = item.w || this.dw;
@@ -134,7 +146,7 @@ export default {
       var bc = this.item.backgroundColor || '#ffffff';
       return `position: absolute;top:${this.item.top}px;left:${this.item.left}px;background-color: ${bc}`
     },
-    lineColor(){
+    lineColor() {
       return '#919191'
     }
   }
@@ -148,10 +160,14 @@ export default {
   z-index: 1000;
   color: red
 }
-.bn-line-action-item{
-  display: none
+
+.bn-line-action-item {
+  display: none;
+  cursor: pointer;
 }
-.bn-line-action:hover .bn-line-action-item{
+
+.bn-line-action:hover .bn-line-action-item {
   display: block;
 }
+
 </style>
